7ca0a2e5aacaff9af74e3903ae8f0a50fb3186c9
Milos Matovic <matovic@gmail.com>
2018-05-26 19:50:52 +0200

    sort words in paragraph

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -6,11 +6,23 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
-  @Input('data') data;
 
-  constructor() { }
+  @Input('data') data: string;
+
+  constructor() {}
 
   ngOnInit() {
+    if(this.data.search(/(<p[^>]+?>|<p>|<\/p>)/ig) > -1) {
+      let re = new RegExp(/(<p[^>]+?>|<p>|<\/p>)|([.,\/#!$%\^&\*;:{}=\-_`~()?])/ig);
+      let newItem = this.data
+        .replace(re, '')
+        .trim()
+        .split(' ')
+        .sort((a,b) => a.localeCompare(b))
+        .join(' ')
+
+      this.data = newItem;
+    }
   }
 
 }
